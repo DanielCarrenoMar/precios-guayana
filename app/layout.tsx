20ced0e04e2318288bc61c0a63f5b0d15d4197ea
DanielCarrenoMar <dacarreno.24@est.ucab.edu.ve>
2025-07-24 10:58:01 -0400

    feat: navbar Global

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
 import NavBarMobile from "@/components/nav-bar-mobile";
+import Navbar from "@/components/nav-bar";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -34,6 +35,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <Navbar />
           {children}
         </ThemeProvider>
       </body>
