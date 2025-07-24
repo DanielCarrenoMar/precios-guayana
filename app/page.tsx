5f37ca4ae1abbc89760c5f42d1b89e82d9b01444
DanielCarrenoMar <dacarreno.24@est.ucab.edu.ve>
2025-07-23 21:44:03 -0400

    feat: quitar componentes de la plantilal y agregar carousel de shancn

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 import { DeployButton } from "@/components/deploy-button";
 import { AuthButton } from "@/components/auth-button";
-import { Hero } from "@/components/hero";
 import { ThemeSwitcher } from "@/components/theme-switcher";
 import Link from "next/link";
-import { getAllProducts, insertOffer } from "@/lib/supabase/repository";
+import { OfferCarousel } from "@/components/offerCarousel";
 
 export default function Home() {
 
@@ -22,10 +21,8 @@ export default function Home() {
           </div>
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-          <Hero />
-          <main className="flex-1 flex flex-col gap-6 px-4">
-            <h2 className="font-medium text-xl mb-4">Next steps</h2>
-          </main>
+          <OfferCarousel />
+          <h1 className="">Prueba de Color</h1>
         </div>
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
