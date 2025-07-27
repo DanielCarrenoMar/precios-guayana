f81b017b715acae4b5cc9774ed29cd8eb6d3a81a
DanielCarrenoMar <dacarreno.24@est.ucab.edu.ve>
2025-07-27 12:15:29 -0400

    feat: terminada pagina usuario en especifico

--- app/user/[userId]/page.tsx
+++ app/user/[userId]/page.tsx
@@ -2,8 +2,9 @@ import OfferCard from "@/components/offerCard";
 import ProductCard from "@/components/productCard";
 import { getOffersByUserId, getProductsByUserId, getUserById } from "@/lib/supabase/repository";
 import { UUID } from "crypto";
-import { Link, LinkIcon, MapPin } from "lucide-react";
+import { LinkIcon, MapPin } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 interface Props {
@@ -37,14 +38,14 @@ export default async function UserPage({ params }: Props) {
                 {/* SECCIÓN PRINCIPAL DE INFORMACIÓN DEL USUARIO */}
                 <header className="bg-primary-foreground p-6 rounded-xl flex flex-col sm:flex-row items-center gap-6">
                     <div className="flex-shrink-0">
-                        {user.imageProfilePath ? 
-                        <Image
-                            src={user.imageProfilePath} // Ruta a un avatar por defecto
-                            alt={`Foto de perfil de ${user.name}`}
-                            width={128}
-                            height={128}
-                            className="rounded-full object-cover border-4 border-gray-700"
-                        />:
+                        {user.imageProfilePath ?
+                            <Image
+                                src={user.imageProfilePath} // Ruta a un avatar por defecto
+                                alt={`Foto de perfil de ${user.name}`}
+                                width={128}
+                                height={128}
+                                className="rounded-full object-cover border-4 border-gray-700"
+                            /> :
                             <div className="size-32 bg-primary text-4xl font-bold text-primary-foreground rounded-full flex justify-center items-center">
                                 {user.name.toUpperCase()[0]}
                             </div>
@@ -55,16 +56,18 @@ export default async function UserPage({ params }: Props) {
                         <p className="text-primary/80 mt-2 max-w-2xl">{user.bios}</p>
 
                         <div className="flex items-center justify-center sm:justify-start gap-6 mt-4 text-sm text-gray-400">
-                            {user.contact && (
-                                <a href={user.contact} target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 hover:text-cyan-400 transition-colors">
+                            {user.contact &&
+                                <span>
                                     <LinkIcon size={16} />
-                                    <span>Contacto</span>
-                                </a>
-                            )}
-                            <Link href="/map" className="flex items-center gap-2 hover:text-cyan-400 transition-colors">
-                                <MapPin size={16} />
-                                <span>Ver en el mapa</span>
-                            </Link>
+                                    <h3>{user.contact}</h3>
+                                </span>
+                            }
+                            {user.latitude && user.longitude &&
+                                <Link href={`/map?lat=${user.latitude}&lng=${user.longitude}`} className="flex items-center gap-2 hover:text-primary transition-colors">
+                                    <MapPin size={16} />
+                                    <span>Ver en el mapa</span>
+                                </Link>
+                            }
                         </div>
                         <p className="text-xs text-gray-500 mt-3">Miembro desde {formatDate(user.created_at)}</p>
                     </div>
