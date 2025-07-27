997ed4fed13e0be2d36fb7ddb8425a4998939b4d
DanielCarrenoMar <dacarreno.24@est.ucab.edu.ve>
2025-07-27 17:58:22 -0400

    feat: no redireciona a tu hubicacion

--- app/map/components/product_map.tsx
+++ app/map/components/product_map.tsx
@@ -8,13 +8,13 @@ import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from "react-leaf
 import L from 'leaflet';
 import Link from 'next/link';
 
-export default function ProductMap({children}: {children?:React.ReactNode}) {
+export default function ProductMap({ children }: { children?: React.ReactNode }) {
     const [products, setProducts] = useState<Product[]>([])
 
     useEffect(() => {
         async function fetchLocationProducts() {
             const data = await (await getAllProducts()).filter(products => products.latitude && products.longitude);
-            setProducts(data);  
+            setProducts(data);
         }
         fetchLocationProducts();
     }, []);
@@ -29,20 +29,20 @@ export default function ProductMap({children}: {children?:React.ReactNode}) {
                 <LocationMarker />
                 {products.map((product) => (
                     product.latitude && product.longitude &&
-                    <Marker key={product.id} position={[product.latitude, product.longitude]}  icon={new L.Icon({
-                    iconUrl: product.imagesPath[0],
-                    iconSize: [38, 38],
-                    iconAnchor: [19, 38],
-                    popupAnchor: [0, -38]
-                })}>
-                    <Popup>
-                        <div className="flex flex-col gap-2">
-                            <h3 className="text-lg font-semibold">{product.title}</h3>
-                            <p>{product.description}</p>
-                            <p className="text-sm text-gray-500">Precio: {product.price} Bs</p>
-                            <Link href={`/product/${product.id}`} className="text-blue-500 hover:underline">Ver detalles</Link>
-                        </div>
-                    </Popup>
+                    <Marker key={product.id} position={[product.latitude, product.longitude]} icon={new L.Icon({
+                        iconUrl: product.imagesPath[0],
+                        iconSize: [38, 38],
+                        iconAnchor: [19, 38],
+                        popupAnchor: [0, -38]
+                    })}>
+                        <Popup>
+                            <div className="flex flex-col gap-2">
+                                <h3 className="text-lg font-semibold">{product.title}</h3>
+                                <p>{product.description}</p>
+                                <p className="text-sm text-gray-500">Precio: {product.price} Bs</p>
+                                <Link href={`/product/${product.id}`} className="text-blue-500 hover:underline">Ver detalles</Link>
+                            </div>
+                        </Popup>
                     </Marker>
                 ))}
             </MapContainer>
@@ -52,18 +52,25 @@ export default function ProductMap({children}: {children?:React.ReactNode}) {
 
 function LocationMarker() {
     const [position, setPosition] = useState<(null | LatLngExpression)>(null)
+
     const map = useMapEvents({
         click() {
             map.locate()
         },
         locationfound(e) {
             setPosition(e.latlng)
-            map.flyTo(e.latlng, map.getZoom())
         },
     })
 
     return position === null ? null : (
-        <Marker position={position}>
+        <Marker position={position} icon={
+            new L.Icon({
+                iconUrl: "/marker-icon.png",
+                iconSize: [24, 32],
+                iconAnchor: [12, 32],
+                popupAnchor: [0, -32],
+            })
+        }>
             <Popup>You are here</Popup>
         </Marker>
     )
